fe80f5d3aedcf4171514f6319ec342958015dd28
tandat198 <tandat198@gmail.com>
2020-09-26 10:22:19 +0700

    update

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
@@ -8,7 +8,8 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { getTestsStart } from "../../redux/test/actions";
 
 const useStyles = makeStyles((theme) => ({
     container: {
@@ -29,7 +30,12 @@ const useStyles = makeStyles((theme) => ({
 
 export default function HomePage() {
     const classes = useStyles();
+    const dispatch = useDispatch();
     const { testsList, isLoading, results } = useSelector((state) => state.test);
+
+    useEffect(() => {
+        dispatch(getTestsStart());
+    }, [dispatch]);
 
     return (
         <Container maxWidth='lg' className={classes.container}>
