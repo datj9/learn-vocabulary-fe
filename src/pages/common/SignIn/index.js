bf8b83c1975a65194116d42930be26da1a3b0aba
Dat Nguyen <tandat198@gmail.com>
2020-12-13 12:50:49 +0700

    fix signin

--- src/pages/common/SignIn/index.js
+++ src/pages/common/SignIn/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import "./style.css";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
